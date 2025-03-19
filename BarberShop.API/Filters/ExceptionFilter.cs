d2b61aadf9d3b840de71f8d9acc9af2f767f1b74
Peter Lourenço <lourenco.pet@gmail.com>
2025-03-19 12:13:02 -0300

    feat: Criação de exception filter

---
+++ BarberShop.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,38 @@
+﻿using BarberShop.Communication.ResponseDTO.Errors;
+using BarberShop.Exception.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace BarberShop.API.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is BarberShopBaseException)
+        {
+            HandleProjectException(context);
+        } else
+        {
+            ThrowUnknownError(context);
+        }
+    }
+    private void HandleProjectException(ExceptionContext context)
+    {
+        if (context.Exception is ErrorOnValidationException)
+        {
+            ErrorOnValidationException exception = (ErrorOnValidationException)context.Exception;
+            var errorMessages = new ResponseErrorDTO(exception.Errors);
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Result = new BadRequestObjectResult(errorMessages);
+        }
+    }
+    private void ThrowUnknownError(ExceptionContext context)
+    {
+        var errorMessages = new ResponseErrorDTO("Unknown Error!");
+
+        context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Result = new ObjectResult(errorMessages);
+    }
+}
